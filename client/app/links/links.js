343a392f247298ade6aa1d9063de28c9ad9ab7e1
seankinzler <sfk27@case.edu>
2016-08-26 20:37:37 -0700

    Passed all tests, connected shorten and links views, implemented authentication.

--- client/app/links/links.js
+++ client/app/links/links.js
@@ -1,5 +1,21 @@
 angular.module('shortly.links', [])
 
-.controller('LinksController', function ($scope, Links) {
-  // Your code here
+.controller('LinksController', function ($scope, $location, Links, Auth) {
+  $scope.init = function() {
+    if (Auth.isAuth()) {
+      $scope.data = {};
+      Links.getAll().then(function(data) {
+        $scope.data.links = data;
+      }); 
+    } else {
+      $location.path('/signin');
+    }
+  }; 
+  $scope.signout = Auth.signout; 
+  $scope.changeView = function() {
+    $location.path('/shorten');
+  };
+
+  $scope.init();
+
 });
--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,17 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
-  // Your code here
+.controller('ShortenController', function ($scope, $location, Links, Auth) {
+  if (Auth.isAuth()) {
+    $scope.link = {}; 
+    $scope.addLink = function(newLink) {
+      Links.addOne(newLink);
+      $scope.url = '';
+    };
+    $scope.signout = Auth.signout;
+    $scope.changeView = function() {
+      $location.path('/links');
+    };
+  } else {
+    $location.path('/signin'); 
+  }
 });
